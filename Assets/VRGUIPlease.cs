79aeb7a5aed530950646631c931441ae1d6029cb
Clare Curtis <clare@140-sfoengwifi55-217.clients.corp.yelpcorp.com>
2015-02-27 14:21:20 -0800

    GUI

---
+++ Assets/VRGUIPlease.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VRGUIPlease : VRGUI 
+{
+	public override void OnVRGUI()
+	{
+		acceptMouse = false;
+		GUI.skin.label.fontSize = 30;
+		GUILayout.BeginArea(new Rect(300, 100, 200, 100));
+		GUILayout.Label("Lives: " + 10);
+		GUILayout.EndArea();
+	}
+}
