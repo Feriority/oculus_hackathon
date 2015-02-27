030a41ecf02aa2fce1b711ad46345aa0733b0de2
Josh Wien <jwien001@gmail.com>
2015-02-27 14:50:44 -0800

    Star counting

--- Assets/VRGUIPlease.cs
+++ Assets/VRGUIPlease.cs
@@ -1,14 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class VRGUIPlease : VRGUI 
+public class VRGUIPlease : VRGUI
 {
+	public OVRPlayerController Controller;
+
 	public override void OnVRGUI()
 	{
 		acceptMouse = false;
 		GUI.skin.label.fontSize = 30;
 		GUILayout.BeginArea(new Rect(300, 100, 200, 100));
-		GUILayout.Label("Lives: " + 10);
+		GUILayout.Label("Stars: " + Controller.GetPickupCount());
 		GUILayout.EndArea();
 	}
 }
